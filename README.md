14a4b0dc501dc66c01091ff58c47900926203957
romeOz <serggalka@gmail.com>
2015-01-26 07:13:48 +0300

    Create project.

---
+++ README.md
@@ -0,0 +1,11 @@
+[READ ONLY] Base module for Rock Framework
+=================
+
+Requirements
+-------------------
+ * **PHP 5.4+**
+
+License
+-------------------
+
+Base module for Rock Framework is open-sourced software licensed under the [MIT license](http://opensource.org/licenses/MIT).
---
+++ tests/bootstrap.php
@@ -0,0 +1,15 @@
+<?php
+$composerAutoload = dirname(__DIR__) . '/vendor/autoload.php';
+if (is_file($composerAutoload)) {
+    /** @var \Composer\Autoload\ClassLoader $loader */
+    $loader = require($composerAutoload);
+}
+
+$loader->addPsr4('rockunit\\', __DIR__);
+
+require(dirname(__DIR__) . '/src/polyfills.php');
+$_SERVER['SERVER_NAME'] = $_SERVER['HTTP_HOST'] = 'site.com';
+$_SERVER['REQUEST_URI'] = '/';
+date_default_timezone_set('UTC');
+
+define('ROCKUNIT_RUNTIME', __DIR__ . '/runtime/');
