5c846a77886a0b33341843cb56f89e8612d7fbb4
romeOz <serggalka@gmail.com>
2015-02-13 03:03:48 +0300

    [skip CI] Added exception messages.

--- src/ExceptionInterface.php
+++ src/ExceptionInterface.php
@@ -27,6 +27,7 @@ interface ExceptionInterface
     const NOT_INSTALL_FILE = 'Library "rock-file" does not install.';
     const NOT_INSTALL_DI = 'Library "rock-di" does not install.';
     const NOT_INSTALL_REQUEST = 'Library "rock-request" does not install.';
+    const NOT_INSTALL_I18N = 'Library "rock-i18n" does not install.';
 
     /**
      * @param string     $msg         message
